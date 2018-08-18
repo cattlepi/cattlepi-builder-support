2473da1d97a8bf0a4c7a7ebffbf335ceb600954c
Mircea <hello@cattlepi.com>
2018-08-18 16:47:35 -0700

    add minimal build script

--- unionfs-fuse/build.sh
+++ unionfs-fuse/build.sh
@@ -2,4 +2,6 @@
 # master build script
 SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export TOPDIR=$SELFDIR
-
+sudo apt-get install  libfuse-dev
+cd $TOPDIR/cache/unionfs-fuse && make
+cd $TOPDIR/cache/unionfs-fuse && sudo make install
